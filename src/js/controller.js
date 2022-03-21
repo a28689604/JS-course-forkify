6bd13f54ddb31118295a56c3094dfd925862f2c2
a28689604 <a28689604@gmail.com>
2022-03-21 15:47:19 +0800

    Add new welcome feature

--- src/js/controller.js
+++ src/js/controller.js
@@ -130,6 +130,10 @@ const controlAddRecipe = async function (newRecipe) {
   }
 };
 
+const newFeature = function () {
+  console.log('welcome!');
+};
+
 const init = function () {
   bookmarksView.addHandlerRender(controlBookmarks);
   recipeView.addHandlerRender(controlRecipe);
@@ -138,6 +142,6 @@ const init = function () {
   searchView.addHandlerSearch(controlSearchResults);
   paginationView.addHandlerClick(controlPagination);
   addRecipeView.addHandlerUpload(controlAddRecipe);
-  console.log('Welcome');
+  newFeature();
 };
 init();
