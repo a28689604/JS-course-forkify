f6b9c6f57739acaab8224251202a27dda4aa524d
a28689604 <a28689604@gmail.com>
2022-03-21 15:31:35 +0800

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '6076d289-bd81-4c71-88c0-de2adebc7abc';
+export const MODAL_CLOSE_SEC = 2.5;
